246fdca470d31c9a1a14742a53fcf8e252b677ff
tianrobot <103897329+tianrobot@users.noreply.github.com>
2023-05-12 21:03:31 +0200

    Add files via upload

--- create_confusion_matrix.py
+++ create_confusion_matrix.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
 
-from utils import read_split_data
+from utils import read_split_data, read_test_data
 from my_dataset import MyDataSet
 from model import swin_base_patch4_window12_384_in22k as create_model
 
@@ -80,34 +80,54 @@ class ConfusionMatrix(object):
         plt.show()
 
 
-def main(args):
+def main(args, str=''):
     device = torch.device(args.device if torch.cuda.is_available() else "cpu")
     print(f"using device: {device}")
-
-    _, _, val_images_path, val_images_label = read_split_data(args.data_path)
 
     img_size = 384
     data_transform = {
         "val": transforms.Compose([transforms.Resize(int(img_size * 1.143)),
                                    transforms.CenterCrop(img_size),
                                    transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+
+        "test": transforms.Compose([transforms.Resize(int(img_size * 1.143)),
+                                   transforms.CenterCrop(img_size),
+                                   transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
-
-    # validation dataset
-    val_dataset = MyDataSet(images_path=val_images_path,
-                            images_class=val_images_label,
-                            transform=data_transform["val"])
-
+    
     nw = min([os.cpu_count(), args.batch_size if args.batch_size > 1 else 0, 8])  # number of workers
     print('Using {} dataloader workers every process'.format(nw))
+    if str == 'val_dataset':
+        _, _, val_images_path, val_images_label = read_split_data(args.data_path)
+        # read validation dataset
+        val_dataset = MyDataSet(images_path=val_images_path,
+                                images_class=val_images_label,
+                                transform=data_transform["val"])
+        
+        # load validation dataset
+        val_loader = torch.utils.data.DataLoader(val_dataset,
+                                                batch_size=args.batch_size,
+                                                shuffle=False,
+                                                pin_memory=True,
+                                                num_workers=nw,
+                                                collate_fn=val_dataset.collate_fn)
+    
+    elif str == 'test_dataset':
+        test_images_path, test_images_label = read_test_data(args.data_path)
+        # read test dataset
+        test_dataset = MyDataSet(images_path=test_images_path,
+                                 images_class=test_images_label,
+                                 transform=data_transform["test"])
 
-    val_loader = torch.utils.data.DataLoader(val_dataset,
-                                             batch_size=args.batch_size,
-                                             shuffle=False,
-                                             pin_memory=True,
-                                             num_workers=nw,
-                                             collate_fn=val_dataset.collate_fn)
-
+        # load test dataset
+        test_loader = torch.utils.data.DataLoader(test_dataset,
+                                                  batch_size=args.batch_size,
+                                                  shuffle=False,
+                                                  pin_memory=True,
+                                                  num_workers=nw,
+                                                  collate_fn=test_dataset.collate_fn)
+    
     model = create_model(num_classes=args.num_classes)
     # load pretrain weights
     assert os.path.exists(args.weights), "cannot find {} file".format(args.weights)
@@ -124,12 +144,20 @@ def main(args):
     confusion = ConfusionMatrix(num_classes=args.num_classes, labels=labels)
     model.eval()
     with torch.no_grad():
-        for val_data in tqdm(val_loader, file=sys.stdout):
-            val_images, val_labels = val_data
-            outputs = model(val_images.to(device))
-            outputs = torch.softmax(outputs, dim=1)
-            outputs = torch.argmax(outputs, dim=1)
-            confusion.update(outputs.to("cpu").numpy(), val_labels.to("cpu").numpy())
+        if str == 'val_dataset':
+            for val_data in tqdm(val_loader, file=sys.stdout):
+                val_images, val_labels = val_data
+                outputs = model(val_images.to(device))
+                outputs = torch.softmax(outputs, dim=1)
+                outputs = torch.argmax(outputs, dim=1)
+                confusion.update(outputs.to("cpu").numpy(), val_labels.to("cpu").numpy())
+        elif str == 'test_dataset':
+            for test_data in tqdm(test_loader, file=sys.stdout):
+                test_images, test_labels = test_data
+                outputs = model(test_images.to(device))
+                outputs = torch.softmax(outputs, dim=1)
+                outputs = torch.argmax(outputs, dim=1)
+                confusion.update(outputs.to("cpu").numpy(), test_labels.to("cpu").numpy())
     confusion.plot()
     confusion.summary()
 
@@ -141,14 +169,15 @@ if __name__ == '__main__':
 
     # The dataset absolute path
     parser.add_argument('--data-path', type=str,
-                        default="./dataset/Training")
+                        default="/Users/tian/Downloads/thesis/Thesis/swin_transformer/dataset/Testing")
 
     # Training weight absolute path
-    parser.add_argument('--weights', type=str, default='../weights/model-6.pth',
+    parser.add_argument('--weights', type=str, default='/Users/tian/Downloads/thesis/Thesis/swin_transformer/weights/model-6.pth',
                         help='initial weights path')
+    
     # Whether to freeze weights
     parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
 
     opt = parser.parse_args()
 
-    main(opt)
+    main(opt, str='test_dataset')
