74ca75bea2355225306f85ceab2f20a404d1e8f9
tianrobot <103897329+tianrobot@users.noreply.github.com>
2023-05-21 15:19:12 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -19,7 +19,7 @@ def main():
                                          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     # load image
-    img_path = ".../dataset/Testing/meningioma_tumor/image(15).jpg"
+    img_path = "/dataset/Testing/meningioma_tumor/image(15).jpg"
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
     img = Image.open(img_path)
     # Convert MRIs into RGB image
@@ -41,7 +41,7 @@ def main():
     # create model
     model = create_model(num_classes=4).to(device)
     # load model weights
-    model_weight_path = "../weights/model-6.pth"
+    model_weight_path = "/weights/model-6.pth"
     model.load_state_dict(torch.load(model_weight_path, map_location=device))
     model.eval()
     with torch.no_grad():
